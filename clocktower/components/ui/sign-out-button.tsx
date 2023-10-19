7966afe681b3e8f6f1620111fc28459587097dcf
Turner Monroe <85597948+turnercore@users.noreply.github.com>
2023-10-19 19:51:37 +0000

    removed old type statement

--- clocktower/components/ui/sign-out-button.tsx
+++ clocktower/components/ui/sign-out-button.tsx
@@ -1,7 +1,5 @@
 "use client"
 import { Button } from '@/components/ui'
-import { Database } from '@/types/supabase'
-import type { SupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/SupabaseAuthClient'
 
 export function SignOutButton(supabase_auth: SupabaseAuthClient) {
