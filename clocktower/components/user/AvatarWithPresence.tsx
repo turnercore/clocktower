915ab184a393d8a641c2efc598201f6b3611ba55
Turner Monroe <85597948+turnercore@users.noreply.github.com>
2023-11-13 02:08:48 +0000

    Able to remove users from a shared tower.

--- clocktower/components/user/AvatarWithPresence.tsx
+++ clocktower/components/user/AvatarWithPresence.tsx
@@ -1,8 +1,22 @@
 // AvatarWithPresence.tsx
 'use client'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui'
+import {
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  toast,
+} from '@/components/ui'
 import hash from '@/tools/hash'
 import { ProfileRow } from '@/types/schemas'
+import { AlertDialog, AlertDialogCancel } from '@radix-ui/react-alert-dialog'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useParams } from 'next/navigation'
+import { useState } from 'react'
 
 interface AvatarWithPresenceProps {
   user: ProfileRow
@@ -15,7 +29,36 @@ const AvatarWithPresence = ({
   isInteractable,
   isOnline,
 }: AvatarWithPresenceProps) => {
-  return (
+  const supabase = createClientComponentClient()
+  const params = useParams()
+  const [isDeleted, setIsDeleted] = useState(false)
+
+  const handleDefenestration = async () => {
+    if (!isInteractable) return
+    if (!params.id) return
+    setIsDeleted(true)
+    // Call supabase to remove user from tower.
+    const { error } = await supabase.rpc('remove_user_from_tower', {
+      userid: user.id,
+      tower: params.id,
+    })
+
+    if (error) {
+      console.error(error)
+      toast({
+        title: 'Error defenestrating user',
+        description: error.message,
+        variant: 'destructive',
+      })
+      setIsDeleted(false)
+    }
+  }
+
+  if (isDeleted) {
+    return null
+  }
+
+  const avatar = (
     <div className='relative inline-block'>
       <Avatar
         onClick={() => {}}
@@ -36,6 +79,27 @@ const AvatarWithPresence = ({
         <span className='w-3 h-3 bg-green-500 absolute bottom-0 right-0 rounded-full border-2 border-white'></span>
       )}
     </div>
+  )
+
+  return isInteractable ? (
+    <AlertDialog>
+      <AlertDialogTrigger>{avatar}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>Are you sure?</AlertDialogHeader>
+        <p>Are you sure you want to defenestrate {user.username}?</p>
+        <AlertDialogFooter>
+          <AlertDialogCancel>🙅‍♀️ Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className='vibrating-element bg-red-500'
+            onClick={handleDefenestration}
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  ) : (
+    avatar
   )
 }
 
