10b53cd42ea78db81c9cece5b1034c978a83f72b
Turner Monroe <me@turnercore.dev>
2023-11-20 20:27:28 +0000

    moved where themeawaresocialicon is

--- clocktower/components/layout/Footer.tsx
+++ clocktower/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import ThemeAwareSocialIcon from '@/components/ThemeAwareSocialIcon'
+import ThemeAwareSocialIcon from '@/components/layout/ThemeAwareSocialIcon'
 
 const Footer = () => {
   const emojis = ['❤️', '🐶', '☕️', '❤️‍🩹', '🤖', '👾', '💻']
